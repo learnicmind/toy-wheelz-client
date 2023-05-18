e69f19e80cf3e2072d8c5457281361e94529f0e1
Ariful Islam Razu <learnicmind@gmail.com>
2023-05-18 23:36:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1>Vite + React</h1>
+      <button className="btn btn-outline">Button</button>
+      <button className="btn btn-outline btn-primary">Button</button>
+      <button className="btn btn-outline btn-secondary">Button</button>
+      <button className="btn btn-outline btn-accent">Button</button>
+
+    </>
+  )
+}
+
+export default App
