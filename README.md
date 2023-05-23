3f91f328be28ac3cd3a56418b54d0f95f84ca609
Ariful Islam Razu <learnicmind@gmail.com>
2023-05-23 23:45:18 +0600

    added readme.md

---
+++ README.md
@@ -0,0 +1,11 @@
+The Best feature of my project is the 'homepage'. Besides the All Toy route is also cool. Some new tool I have used for this project-
+* Firebase
+* react-icons
+* react-rating
+* react-tabs
+* sort-by
+* sweetalert2
+* tailwind css with daisyUI
+
+Live Link of my project-
+* https://toy-wheelz-client.web.app/
--- src/pages/Home/Shipping/Shipping.jsx
+++ src/pages/Home/Shipping/Shipping.jsx
@@ -4,25 +4,25 @@ const Shipping = () => {
     return (
         <div>
             <h1 className='text-3xl font-bold text-center py-3'>Our Shipping methods</h1>
-            <p className='text-slate-500 text-center'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus adipisci ipsum similique recusandae impedit nihil voluptatibus quam commodi at beatae! Deleniti ipsum sunt rerum pariatur velit odit, animi sed esse?</p>
+            <p className='text-slate-500 text-center px-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus adipisci ipsum similique recusandae impedit nihil voluptatibus quam commodi at beatae! Deleniti ipsum sunt rerum pariatur velit odit, animi sed esse?</p>
 
-            <div className='grid md:grid-cols-3 items-center justify-between gap-10 my-8 bg-slate-200 p-8 rounded-lg'>
+            <div className='grid md:grid-cols-3 items-center justify-between gap-10 my-8 bg-blue-500 p-8 mx-8'>
                 <div className='space-y-2'>
-                    <img className='w-28 rounded' src="https://l.facebook.com/l.php?u=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%253AANd9GcRJhRSEpLKFeeDTd86RJ_oyc0lEK2c4OubWtA%26usqp%3DCAU%26fbclid%3DIwAR0jCcYNEYgg8kw34wPt5wumHIDodtquou1GxFiuYzpz3LMwmfAm6STIQeU&h=AT01FpLBvNPgMQBFDz4Mj2qiqwxAQqRqsx4upj5CejFrXUa5jn3d1xIl2w9Q3xMOtLHNh5egPlWbI2vvGxTRSMCnIyKxT4lc2iGcCyJoXc67evrDfuG6FLmtkdha9NoSz3Vs4Q" alt="" />
+                    <img className='w-28 rounded' src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRJhRSEpLKFeeDTd86RJ_oyc0lEK2c4OubWtA&usqp=CAU&fbclid=IwAR0jCcYNEYgg8kw34wPt5wumHIDodtquou1GxFiuYzpz3LMwmfAm6STIQeU" alt="" />
 
                     <h1 className='text-xl font-bold text-white'>Shipping Abroad</h1>
                     <p className='text-slate-300'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse iure voluptates nulla excepturi eligendi tempore!</p>
                 </div>
 
                 <div className='space-y-2'>
-                    <img className='w-24 rounded' src="https://static.vecteezy.com/system/resources/prevhttps://l.facebook.com/l.php?u=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%253AANd9GcR4ksePXnFt2g0ZdFNnqgYaf631t_pOO2J9xQ%26usqp%3DCAU%26fbclid%3DIwAR1D6Tyhe8homYOb9DID2KU1Aq1jVhUiLZhkUXxnnv3kYtf0thnzvOv2Rt0&h=AT01FpLBvNPgMQBFDz4Mj2qiqwxAQqRqsx4upj5CejFrXUa5jn3d1xIl2w9Q3xMOtLHNh5egPlWbI2vvGxTRSMCnIyKxT4lc2iGcCyJoXc67evrDfuG6FLmtkdha9NoSz3Vs4Qiews/001/760/473/original/delivery-service-composition-with-truck-and-packages-vector.jpg" alt="" />
+                    <img className='w-24 rounded' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4ksePXnFt2g0ZdFNnqgYaf631t_pOO2J9xQ&usqp=CAU" alt="" />
 
                     <h1 className='text-xl font-bold text-white'>Shipping All Over Country</h1>
                     <p className='text-slate-300'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse iure voluptates nulla excepturi eligendi tempore!</p>
                 </div>
 
                 <div className='space-y-2'>
-                    <img className='w-20 rounded' src="https://l.facebook.com/l.php?u=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%253AANd9GcSYwR6eTnXs0BpHpMx1zPFDJnGe2saGxJBi7Q2WJD5NVufIzYG_RGTHWt6UqxY-rqSDuxY%26usqp%3DCAU%26fbclid%3DIwAR2lwEc-svymolepAF4MtOcoTmzA7hMYqYaSZQrVT7I9pu6per7rslpjiQs&h=AT01FpLBvNPgMQBFDz4Mj2qiqwxAQqRqsx4upj5CejFrXUa5jn3d1xIl2w9Q3xMOtLHNh5egPlWbI2vvGxTRSMCnIyKxT4lc2iGcCyJoXc67evrDfuG6FLmtkdha9NoSz3Vs4Q" alt="" />
+                    <img className='w-20 rounded' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYwR6eTnXs0BpHpMx1zPFDJnGe2saGxJBi7Q2WJD5NVufIzYG_RGTHWt6UqxY-rqSDuxY&usqp=CAU" alt="" />
 
                     <h1 className='text-xl font-bold text-white'>Home Delivery Service</h1>
                     <p className='text-slate-300'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse iure voluptates nulla excepturi eligendi tempore!</p>
